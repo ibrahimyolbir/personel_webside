fe1e93368657c21f8ebe46ed2b163e3fd1d8d718
ibrahimyolbir <ibrahimyolbirr@gmail.com>
2018-02-12 22:26:12 +0100

    navigation scroll added

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+
+$(document).ready(function (){
+    $('.nav_href').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $(this).attr('href') ).offset().top
+        }, 1000);
+        return false;
+    });
+});
