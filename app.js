05559396f78ff9775fce143530f2ff621ab860fe
ibrahimyolbir <ibrahimyolbirr@gmail.com>
2018-02-20 17:22:54 +0100

    added portfolio and edited Nav_Bar

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-
-
-
+//Kaydirma hizini ayarlama
 $(document).ready(function (){
     $('.nav_href').click(function(){
         $('html, body').animate({
             scrollTop: $( $(this).attr('href') ).offset().top
         }, 1000);
+        return false;
+    });
+});
+// Yukari kaydirma Buton foksiyonu
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 100)    // Sayfa ne kadar aşağı kayarsa buton görünsün. 100 sayısı = Kaydırma çubuğunun piksel konumu. Bu sayı değiştirilebilir.
+        $("#yukari").fadeIn(500);    // Yukarı çık butonu ne kadar hızla ortaya çıksın. 500 milisaniye = 0,5 saniye. Bu sayı değiştirilebilir.
+    else 
+        $("#yukari").fadeOut(500);    // Yukarı çık butonu ne kadar hızla ortadan kaybolsun. 500 milisaniye = 0,5 saniye. Bu sayı değiştirilebilir.
+});
+$(document).ready(function(){
+    $("#yukari").click(function(){    // Yukarı çık butonuna tıklanıldığında aşağıdaki satır çalışır.
+        $("html, body").animate({ scrollTop: "0" }, 500);    // Sayfa ne kadar hızla en yukarı çıksın.
+        // 0 sayısı sayfanın en üstüne çıkılacağını belirtir.
+        // 500 sayısı ne kadar hızla çıkılacağını belirtir. 500 milisaniye = 0,5 saniye. Bu sayı değiştirilebilir.
         return false;
     });
 });
